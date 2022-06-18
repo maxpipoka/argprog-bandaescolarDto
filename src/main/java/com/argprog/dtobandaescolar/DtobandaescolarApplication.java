20d6cb61074efbb0dbd165e38aec1ce88f7f8650
maxpipoka <martincardozo@gmail.com>
2022-06-18 20:24:25 -0300

    First commit

---
+++ src/main/java/com/argprog/dtobandaescolar/DtobandaescolarApplication.java
@@ -0,0 +1,13 @@
+package com.argprog.dtobandaescolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtobandaescolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtobandaescolarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/argprog/dtobandaescolar/DtobandaescolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argprog.dtobandaescolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtobandaescolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
